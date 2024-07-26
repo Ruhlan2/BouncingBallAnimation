98ae6dc093fc3358688a8db740fd73e69d2bc995
ruhlandev <r.usubov@expressbank.az>
2024-07-26 23:59:27 +0400

    completed test part with CI

--- app/src/main/java/com/example/bouncingballanimation/MainActivity.kt
+++ app/src/main/java/com/example/bouncingballanimation/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.bouncingballanimation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -48,6 +49,7 @@ class MainActivity : ComponentActivity() {
                             )
                         )
                 ) { innerPadding ->
+                    Log.d("TAG", "onCreate() called with: innerPadding = $innerPadding")
                     BouncingBallScreen()
                 }
             }
